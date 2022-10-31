6ee463d931b4a179d41e30a221fefa2597cdb7a0
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-10-31 11:19:29 +0530

    Add Css

--- web/calc/templatetags/calc_tags.py
+++ web/calc/templatetags/calc_tags.py
@@ -7,26 +7,22 @@ register = template.Library()
 @register.simple_tag
 def sum(num1, num2):
     result = num1 + num2
-    print(result)
     return result
 
 
 @register.simple_tag
 def sub(num1, num2):
     result = num1 - num2
-    print(result)
     return result
 
 
 @register.simple_tag
 def mul(num1, num2):
     result = num1 * num2
-    print(result)
     return result
 
 
 @register.simple_tag
 def div(num1, num2):
     result = num1 / num2
-    print(result)
     return result
